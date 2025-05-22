1c1f499dccea2d7bbaecdf85e2e05a0d0ec79e6f
hjjhfb <hjjhfb@gmail.com>
2025-05-22 12:32:52 +0200

    Add files via upload

--- header.js
+++ header.js
@@ -2,4 +2,6 @@ fetch('header.html')
   .then(response => response.text())
   .then(data => {
     document.getElementById('header-include').outerHTML = data;
+    document.body.classList.add('page-loaded');
   });
+  
